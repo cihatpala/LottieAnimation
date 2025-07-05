0bdeab61483c8b309d6dc7bbf816a21c3974f17a
Cihat Pala <cihat_pala@hotmail.com>
2025-07-05 23:04:33 +0300

    Set Robolectric SDK to 34

--- app/src/test/java/com/cihat/egitim/lottieanimation/ExampleUnitTest.kt
+++ app/src/test/java/com/cihat/egitim/lottieanimation/ExampleUnitTest.kt
@@ -8,11 +8,13 @@ import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.performClick
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.junit.runner.RunWith
+import org.robolectric.annotation.Config
 import org.junit.Assert.assertTrue
 import org.junit.Rule
 import org.junit.Test
 
 @RunWith(AndroidJUnit4::class)
+@Config(sdk = [34])
 class ExampleUnitTest {
     @get:Rule
     val composeRule = createComposeRule()
