669d6bb8dee7247550fcf64dc67a15f66eb789b4
Cihat Pala <cihat_pala@hotmail.com>
2025-07-05 22:42:10 +0300

    Remove unused size import

--- app/src/main/java/com/cihat/egitim/lottieanimation/MainActivity.kt
+++ app/src/main/java/com/cihat/egitim/lottieanimation/MainActivity.kt
@@ -13,7 +13,6 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
