573cb8b6b2cfc0f30e1d0b7badd4fd49d4ca76e1
Cihat Pala <cihat_pala@hotmail.com>
2025-07-07 23:47:36 +0300

    Update color scheme for light and dark themes

--- app/src/main/java/com/cihat/egitim/lottieanimation/ui/theme/Color.kt
+++ app/src/main/java/com/cihat/egitim/lottieanimation/ui/theme/Color.kt
@@ -2,10 +2,44 @@ package com.cihat.egitim.lottieanimation.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+// Light theme colors
+val LightPrimary = Color(0xFF0061A4)
+val LightOnPrimary = Color(0xFFFFFFFF)
+val LightPrimaryContainer = Color(0xFFD2E4FF)
+val LightOnPrimaryContainer = Color(0xFF001D36)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val LightSecondary = Color(0xFF535F70)
+val LightOnSecondary = Color(0xFFFFFFFF)
+val LightSecondaryContainer = Color(0xFFD7E3F8)
+val LightOnSecondaryContainer = Color(0xFF101C2B)
+
+val LightTertiary = Color(0xFF705574)
+val LightOnTertiary = Color(0xFFFFFFFF)
+val LightTertiaryContainer = Color(0xFFFAD8FF)
+val LightOnTertiaryContainer = Color(0xFF2B152D)
+
+val LightBackground = Color(0xFFFBFCFF)
+val LightOnBackground = Color(0xFF1A1C1E)
+val LightSurface = Color(0xFFFBFCFF)
+val LightOnSurface = Color(0xFF1A1C1E)
+
+// Dark theme colors
+val DarkPrimary = Color(0xFF9FC9FF)
+val DarkOnPrimary = Color(0xFF003259)
+val DarkPrimaryContainer = Color(0xFF00497F)
+val DarkOnPrimaryContainer = Color(0xFFD2E4FF)
+
+val DarkSecondary = Color(0xFFBBC7DB)
+val DarkOnSecondary = Color(0xFF253140)
+val DarkSecondaryContainer = Color(0xFF3B4858)
+val DarkOnSecondaryContainer = Color(0xFFD7E3F8)
+
+val DarkTertiary = Color(0xFFDDBCE3)
+val DarkOnTertiary = Color(0xFF412742)
+val DarkTertiaryContainer = Color(0xFF593D5A)
+val DarkOnTertiaryContainer = Color(0xFFFAD8FF)
+
+val DarkBackground = Color(0xFF1A1C1E)
+val DarkOnBackground = Color(0xFFE3E2E6)
+val DarkSurface = Color(0xFF1A1C1E)
+val DarkOnSurface = Color(0xFFE3E2E6)
