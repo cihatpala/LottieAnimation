deb8d1bc7e9c8fdb9fe36247a344ed6c0b37ab84
CihatWindows <cihat_pala@hotmail.com>
2025-07-25 22:07:33 +0300

    fix

--- app/src/main/java/com/cihat/egitim/lottieanimation/ui/screens/AuthScreen.kt
+++ app/src/main/java/com/cihat/egitim/lottieanimation/ui/screens/AuthScreen.kt
@@ -17,6 +17,7 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.sizeIn
 import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Icon
